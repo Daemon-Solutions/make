4e938cf5d8975311bdef527faf2321b0c2d5da70
Gary Rutland <gary.rutland@dae.mn>
2025-07-31 10:56:05 +0100

    Comment out aws_instance resource in example
    The aws_instance resource block in examples/tflint/main.tf has been commented out, likely to prevent errors due to the invalid instance type. Instructions were added to uncomment the block to reproduce the error if needed.

--- examples/tflint/main.tf
+++ examples/tflint/main.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = "~> 1.0"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0ff8a91507f77f867"
-  instance_type = "t1.2xlarge" # invalid type!
-}
+# uncomment the following block to see the error
+# resource "aws_instance" "foo" {
+#   ami           = "ami-0ff8a91507f77f867"
+#   instance_type = "t1.2xlarge" # invalid type!
+# }
